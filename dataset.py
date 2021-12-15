fb852d92783936090645ef07b40b6a4c3732c683
Matej Berezny <mberezny@redhat.com>
2021-12-15 20:42:33 +0100

    dataset: added support of multiple workers

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import threading
 import pandas as pd
 import numpy as np
 import json
@@ -7,34 +8,39 @@ from pymongo import MongoClient
 
 class Dataset:
 
-    def __init__(self, name):
+    types = {'date': np.datetime64,
+             'integer': np.uint32,
+             'number': np.uint32,
+             'string': str,
+             'boolean': bool}
+    region_mapper = {'CZ010': 'Hlavní město Praha',
+                     'CZ020': 'Středočeský kraj',
+                     'CZ031': 'Jihočeský kraj',
+                     'CZ032': 'Plzeňský kraj',
+                     'CZ041': 'Karlovarský kraj',
+                     'CZ042': 'Ústecký kraj',
+                     'CZ051': 'Liberecký kraj',
+                     'CZ052': 'Královéhradecký kraj',
+                     'CZ053': 'Pardubický kraj',
+                     'CZ063': 'Kraj Vysočina',
+                     'CZ064': 'Jihomoravský kraj',
+                     'CZ071': 'Olomoucký kraj',
+                     'CZ072': 'Zlínský kraj',
+                     'CZ080': 'Moravskoslezský kraj'}
+
+
+    def __init__(self, name, workers):
         """Sets up the connection between remote db and local client.
 
         Args:
             name (str): mongo database name
         """
-        self.types = {'date': np.datetime64,
-                      'integer': np.uint32,
-                      'number': np.uint32,
-                      'string': str,
-                      'boolean': bool}
         self.name = name
-        self.client = MongoClient("mongodb://ubuntu:klat8klat@ec2-13-40-24-161.eu-west-2.compute.amazonaws.com:27017/{}?authSource=admin".format(self.name))
+        self.connection_string = "mongodb://ubuntu:klat8klat@ec2-13-40-24-161.eu-west-2.compute.amazonaws.com:27017/{}?authSource=admin".format(self.name)
+        self.workers = workers
+        self.client = MongoClient(self.connection_string)
         self.db = self.client[self.name]
-        self.region_mapper = {'CZ010': 'Hlavní město Praha',
-                              'CZ020': 'Středočeský kraj',
-                              'CZ031': 'Jihočeský kraj',
-                              'CZ032': 'Plzeňský kraj',
-                              'CZ041': 'Karlovarský kraj',
-                              'CZ042': 'Ústecký kraj',
-                              'CZ051': 'Liberecký kraj',
-                              'CZ052': 'Královéhradecký kraj',
-                              'CZ053': 'Pardubický kraj',
-                              'CZ063': 'Kraj Vysočina',
-                              'CZ064': 'Jihomoravský kraj',
-                              'CZ071': 'Olomoucký kraj',
-                              'CZ072': 'Zlínský kraj',
-                              'CZ080': 'Moravskoslezský kraj'}
+
 
     def clear(self, collection_name=None):
         """Clears one collection from remote db if collection_name is specified, otherwise wipes the entire database. 
@@ -59,11 +65,12 @@ class Dataset:
             schema_url (str): link for downloading schema for aforementioned data
             update (bool): if True, updates the existing table instead of only downloading new one
         """
-        print(f"Processing {name}... ", end=' ')
+        print(f"Processing {name}... ")
         schema_name = f"{name}_schema"
 
         schema = self.get(schema_name)
-        
+        old_data = None
+
         if not schema:
             schema = requests.get(schema_url, headers={'user-agent' : ""}).content.decode('utf-8-sig')
             schema = json.loads(schema)['tableSchema']
@@ -74,40 +81,31 @@ class Dataset:
 
         data = pd.read_csv(url, index_col=0)
         data.index.names = ['_id']
-        data = self.parse(data, schema)
 
         if update:
             old_data = self.get_dataframe(name)
-            data = data[~data.isin(old_data)].dropna()
 
-        self.insert(data, name)
+        data = np.array_split(data, self.workers)
+
+        threads = []
+
+        for idx, item in enumerate(data):
+            threads.append(ProcessingThread(conn_string=self.connection_string,
+                                            db_name=self.name, 
+                                            name=name,
+                                            idx=idx,
+                                            data=item,
+                                            schema = schema,
+                                            update=update,
+                                            old_data=old_data))
+            threads[idx].start()
+
+        for t in threads:
+            t.join()
+
         print("Done.")
 
 
-    def parse(self, data, schema):
-        """Transforms datatypes loaded from db/.csv files to more fitting ones according to the 
-        informations stored in schema collections.
-
-        Args:
-            data (pd.DataFrame): dataframe containing data
-            schema (dict): schema dictionary containing column:datatype pairs
-
-        Returns:
-            pd.DataFrame: processed dataframe with correct datatypes
-        """
-        data = data.fillna(0)
-        data = data.astype({item['name']: self.types[item['datatype']] for item in schema['columns']})
-        
-        if 'kraj_nuts_kod' in data:
-            data = data.replace({'kraj_nuts_kod': self.region_mapper})
-
-        if 'datum' in data:
-            data['datum'] = pd.to_datetime(data['datum'])
-        elif 'casref_do' in data:
-            data['casref_do'] = pd.to_datetime(data['casref_do'])
-
-        return data
-    
 
     def __save_schema(self, schema, name):
         """Stores schema as a whole into the remote db.
@@ -118,6 +116,7 @@ class Dataset:
         """
         table = self.db[name]
         table.insert_one(schema)
+
 
 
     def get_dataframe(self, name):
@@ -149,13 +148,69 @@ class Dataset:
         return data
 
 
-    def insert(self, data, name):
+    @classmethod
+    def parse(cls, data, schema):
+        """Transforms datatypes loaded from db/.csv files to more fitting ones according to the 
+        informations stored in schema collections.
+
+        Args:
+            data (pd.DataFrame): dataframe containing data
+            schema (dict): schema dictionary containing column:datatype pairs
+
+        Returns:
+            pd.DataFrame: processed dataframe with correct datatypes
+        """
+        data = data.fillna(0)
+        data = data.astype({item['name']: cls.types[item['datatype']] for item in schema['columns']})
+        
+        if 'kraj_nuts_kod' in data:
+            data = data.replace({'kraj_nuts_kod': cls.region_mapper})
+
+        if 'datum' in data:
+            data['datum'] = pd.to_datetime(data['datum'])
+        elif 'casref_do' in data:
+            data['casref_do'] = pd.to_datetime(data['casref_do'])
+
+        return data
+
+    @classmethod
+    def insert(cls, data, table):
         """Inserts dataframe into the remote db.
 
         Args:
             data (pd.DataFrame): dataframe containing data
             name (str): name under which collection will be stored in db
         """
+        
         if not data.empty:
-            table = self.db[name]
             table.insert_many(data.reset_index().to_dict('records'))
+        
+
+class ProcessingThread(threading.Thread):
+
+
+    def __init__(self, conn_string, db_name, name, idx, data, schema, 
+                 update=False, old_data=None):
+        threading.Thread.__init__(self)
+        self.client = MongoClient(conn_string)
+        self.db = self.client[db_name]
+        self.name = name
+        self.idx = idx
+        self.schema = schema
+        self.data = data
+        self.update = update
+        self.old_data = old_data
+
+
+    def run(self):
+
+        data = Dataset.parse(self.data, self.schema)
+
+        if self.update:
+            data = data[~data.isin(self.old_data)].dropna()
+
+        table = self.db[self.name]
+        Dataset.insert(data, table)
+
+        print(f"Worker {self.idx} has finished uploading {self.name}")
+
--- download.py
+++ download.py
@@ -7,16 +7,18 @@ parser.add_argument('-u', '--update', nargs='+', type=str, help="Updates the spe
 parser.add_argument('-d', '--download', nargs='+', type=str, help="Clears the database and downloads datasets stored in datasets.json")
 parser.add_argument('-c', '--clear', action="store_true", help="Clears the database")
 parser.add_argument('-s', '--fetch', type=str, help="Fetches the specified collection from db")
+parser.add_argument('-w', '--workers', type=int, default=2, help="Fetches the specified collection from db")
+
 args = parser.parse_args()
 
 #Load json file with all the urls/collection_names
 with open('datasets.json') as file:
     metadata = json.load(file)
 
-dataset = Dataset(name='proj_db')
+dataset = Dataset(name='proj_db', workers=args.workers)
 
 #Clear the db if specified in params or if full download is performed
-if args.clear or args.download:
+if args.clear:
     dataset.clear()
 
 
@@ -25,6 +27,7 @@ if args.download:
     download_all = len(args.download) == 1 and args.download[0] == 'all'
     for item in metadata['sources']:
         if download_all or item['name'] in args.download:
+            dataset.clear(item['name'])
             dataset.download_and_insert(**item)
 elif args.update:
     update_all = len(args.update) == 1 and args.update[0] == 'all'
